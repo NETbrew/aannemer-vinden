3c3fdd362bd318eaabadb38899d2463a3fe128e3
NETbrew <113974723+cesarvanleuffelen@users.noreply.github.com>
2024-11-21 17:26:13 +0100

    logic for adding to database...

--- app/api/lead/route.ts
+++ app/api/lead/route.ts
@@ -21,12 +21,11 @@ export async function POST(req: NextRequest) {
 		// Here you can add your own logic
 		// For instance, sending a welcome email (use the the sendEmail helper function from /libs/mailgun)
 		// For instance, saving the lead in the database (uncomment the code below)
+		const lead = await Lead.findOne({ email: body.email });
 
-		// const lead = await Lead.findOne({ email: body.email });
-
-		// if (!lead) {
-		// 	await Lead.create({ email: body.email });
-		// }
+		if (!lead) {
+			await Lead.create({ email: body.email });
+		}
 
 		return NextResponse.json({});
 	} catch (e) {
